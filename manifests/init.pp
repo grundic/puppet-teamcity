e5ee20d424353e4727b694a7a5318ea2cfefc889
g.chernyshev <systray@yandex.ru>
2012-10-16 08:37:36 +0400

    initial module structure

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: teamcity
+#
+# This module manages teamcity
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class teamcity {
+
+
+}
