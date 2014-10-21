5734bdfe6e861a216899bed89d8a0697c0f68d00
g.chernyshev <systray@yandex.ru>
2014-10-22 00:12:56 +0400

    add puppet doc for teamcity::agent

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,4 +1,74 @@
-
+# == Class: teamcity::agent
+#
+# This class would install and configure Teamcity build agent.
+#
+# === Parameters
+# [*agent_name*]
+#   Build agent name, that will be displayed on Teamcity server.
+#   Defaults to hostname.
+#
+# [*agent_user*]
+#   User name, which will be used to run and configure build agent.
+#   Defaults to `teamcity`.
+#
+# [*agent_user_home*]
+#   If user is managed, set it's home dir.
+#   Defaults to $agent_dir.
+#
+# [*manage_agent_user_home*]
+#   Manage whether to create home dir for user.
+#   Defaults to `false`.
+#
+# [*agent_group*]
+#   Name of group to be used for build agent.
+#   Defaults to `teamcity`.
+#
+# [*manage_user*]
+#   Whether to create user for build agent.
+#   Defaults to `false`.
+#
+# [*manage_group*]
+#   Whether to create group for build agent.
+#   Defaults to `false`.
+#
+# [*server_url*]
+#   Root url of Teamcity server. It will be used in agent's config.
+#   Defaults to `http://builder`.
+#
+# [*archive_name*]
+#   Name of archive that should be downloaded from `server_url`.
+#   Defaults to `buildAgent.zip`.
+#
+# [*download_url*]
+#   Calculated parameter, that is used to download build agent distributive.
+#   Defaults to `${server_url}/update/${archive_name}`.
+#
+# [*agent_dir*]
+#   Installation path, where build agent will reside.
+#   Defaults to `/opt/build-agent`.
+#
+# [*service_ensure*]
+#   Required service status.
+#   Defaults to `running`.
+#
+# [*service_enable*]
+#   Should the service be enabled
+#   Defaults to `true`.
+#
+# [*teamcity_agent_mem_opts*]
+#   String for configuring additional java parameters for build agent
+#
+# [*custom_properties*]
+#   Hash of custom properites, that will be applied to buildAgent.properites
+#
+#
+# === Examples
+#
+# === Authors
+# grundic
+#
+# === Copyright
+# Copyright 2012-2014 Grigory Chernyshev
 class teamcity::agent (
   $agent_name              = $teamcity::params::agent_name,
 
