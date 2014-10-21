b31403e4ca670eb46d4037118668137b4ca35635
g.chernyshev <systray@yandex.ru>
2014-10-21 14:12:00 +0400

    fix parameters names, reorder dependencies

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,31 +1,31 @@
 
 class teamcity::agent (
-  $agent_name              = $::teamcity::params::agent_name,
+  $agent_name              = $teamcity::params::agent_name,
 
-  $agent_user              = $::teamcity::params::agent_user,
-  $agent_user_home         = $::teamcity::params::agent_user_home,
-  $manage_agent_user_home  = $::teamcity::params::manage_agent_user_home,
-  $agent_group             = $::teamcity::params::agent_group,
-  $manage_user             = $::teamcity::params::manage_user,
-  $manage_group            = $::teamcity::params::manage_group,
+  $agent_user              = $teamcity::params::agent_user,
+  $agent_user_home         = $teamcity::params::agent_user_home,
+  $manage_agent_user_home  = $teamcity::params::manage_agent_user_home,
+  $agent_group             = $teamcity::params::agent_group,
+  $manage_user             = $teamcity::params::manage_user,
+  $manage_group            = $teamcity::params::manage_group,
 
-  $server_url              = $::teamcity::params::server_url,
-  $archive_name            = $::teamcity::params::archive_name,
-  $download_url            = $::teamcity::params::download_url,
-  $agent_dir               = $::teamcity::params::agent_dir,
+  $server_url              = $teamcity::params::server_url,
+  $archive_name            = $teamcity::params::archive_name,
+  $download_url            = $teamcity::params::download_url,
+  $agent_dir               = $teamcity::params::agent_dir,
 
-  $service_ensure          = $::teamcity::params::service_ensure,
-  $service_enable          = $::teamcity::params::service_enable,
-  $teamcity_agent_mem_opts = $::teamcity::params::teamcity_agent_mem_opts,
-  $custom_properties       = $::teamcity::params::custom_properties
+  $service_ensure          = $teamcity::params::service_ensure,
+  $service_enable          = $teamcity::params::service_enable,
+  $teamcity_agent_mem_opts = $teamcity::params::teamcity_agent_mem_opts,
+  $custom_properties       = $teamcity::params::custom_properties
 ) inherits ::teamcity::params {
 
   validate_string($agent_name)
 
   Exec { path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin' ] }
 
+  Class['teamcity::agent'] ->
   class {'::teamcity::agent::install':} ->
   class {'::teamcity::agent::config':} ~>
-  class {'::teamcity::agent::service':} ->
-  Class['teamcity::agent']
+  class {'::teamcity::agent::service':}
 }
