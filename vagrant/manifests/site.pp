74cfcbd76772c338305001a0176e5221035fa998
g.chernyshev <systray@yandex.ru>
2015-08-17 15:42:52 +0300

    Update vagrant to support Windows

--- vagrant/manifests/site.pp
+++ vagrant/manifests/site.pp
@@ -1,14 +1,34 @@
-package {'unzip':
-  ensure => installed
-}
 
+if $::kernel == 'windows' {
+  include windows::java
 
-class {'::teamcity::agent':
-  agent_name        => 'sample-build-agent',
-  manage_user       => true,
-  manage_group      => true, 
-  custom_properties => {
-      "system.teamcity.idea.home" => "%system.agent.home.dir%/tools/idea"
+  class {'teamcity::agent':
+    agent_dir             => 'C:/buildAgent',
+    service_run_type      => 'standalone',
+    agent_user            => 'vagrant',
+    launcher_wrapper_conf => {
+      'wrapper.app.parameter.11' => '-Dfile.encoding=UTF-8'
+    }
   }
 }
+else {
+  package {'unzip':
+    ensure => installed
+  }
 
+  class { 'java':
+    distribution => 'jre',
+  }
+
+  class {'::teamcity::agent':
+    agent_name            => 'sample-build-agent',
+    manage_user           => true,
+    manage_group          => true,
+    custom_properties     => {
+        'system.teamcity.idea.home' => '%system.agent.home.dir%/tools/idea'
+    },
+    launcher_wrapper_conf => {
+      'wrapper.app.parameter.11' => '-Dfile.encoding=UTF-8'
+    }
+  }
+}
