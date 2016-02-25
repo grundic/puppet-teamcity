d210ce619595b6b99b10cf9791c7ca05ad60e452
Grigory Chernyshev <systray@yandex.ru>
2016-02-25 01:02:29 +0100

    Complete tests for installation

--- spec/classes/teamcity_spec.rb
+++ spec/classes/teamcity_spec.rb
@@ -11,12 +11,37 @@ describe 'teamcity', :type => 'class' do
       end
 
       context 'installation class tests' do
-        it { should contain_wget__fetch('teamcity-buildagent') }
+        it { should contain_wget__fetch('teamcity-buildagent').with(
+          'source' => 'http://builder/update/buildAgent.zip',
+        # 'destination' => '/tmp/buildAgent.zip'  # TODO: tmp doesn't work on Mac
+        ) }
 
+        it { should contain_exec('extract-agent-archive').with(
+          # 'command' => 'unzip /tmp/buildAgent.zip -d /opt/build-agent' # TODO `tmp` doesn't work on Mac
+          'creates' => '/opt/build-agent/conf'
+        ).that_requires('Wget::Fetch[teamcity-buildagent]') }
 
-        # Base files
-        it { should contain_file('agent-config') }
-        it { should contain_file('/opt/build-agent/bin/agent.sh') }
+        it { should contain_file('agent-config').
+          with(
+            'ensure' => 'present',
+            'path' => '/opt/build-agent/conf/buildAgent.properties',
+            'replace' => 'no',
+            'group' => 'teamcity',
+            'owner' => 'teamcity',
+          ).
+          that_requires('Exec[extract-agent-archive]')
+        }
+
+        it { should contain_exec('chown-agent-dir').with(
+          'command' => 'chown -R teamcity:teamcity /opt/build-agent'
+        ).that_requires('Exec[extract-agent-archive]').
+          that_subscribes_to('Exec[extract-agent-archive]')
+        }
+
+        it { should contain_file('/opt/build-agent/bin/agent.sh').
+          with('mode' => '0755').
+          that_requires('Exec[extract-agent-archive]')
+        }
       end
 
     end
